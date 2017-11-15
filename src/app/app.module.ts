e028385997c859e055f6667b2ab44a5fb028ac25
Valeriia Parfeliuk <parfelyuk.v@gmail.com>
2017-11-15 13:59:03 +0200

    Added navbar and defined the routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ZumbaComponent } from './group-class/zumba/zumba.component';
+import { PilatesComponent } from './group-class/pilates/pilates.component';
+import { YogaComponent } from './group-class/yoga/yoga.component';
+import { CycleComponent } from './group-class/cycle/cycle.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { PersonalTrainingComponent } from './personal-training/personal-training.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ZumbaComponent,
+    PilatesComponent,
+    YogaComponent,
+    CycleComponent,
+    PersonalTrainingComponent,
+    ContactUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'zumba', component: ZumbaComponent},
+      { path: 'pilates', component: PilatesComponent},
+      { path: 'yoga', component: YogaComponent},
+      { path: 'cycle', component: CycleComponent},
+      { path: 'personal-training', component: PersonalTrainingComponent},
+      { path: 'contact-us', component: ContactUsComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/group-class/zumba/zumba.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zumba',
+  templateUrl: './zumba.component.html',
+  styleUrls: ['./zumba.component.css']
+})
+export class ZumbaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
